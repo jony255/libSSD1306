57c6fe9ed2507115bd34a44f57c2172bccdbcc93
Jony <maybe-one-day-ubermensch@protonmail.com>
2021-09-04 16:40:04 -0700

    scripts: add script to download SSD1306 datasheet
    I don't know if I can distribute the pdf within the repo so adding a
    script to download it should be fine.

---
+++ scripts/dl_ssd1306_datasheet.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+DATASHEET_URL='https://cdn-shop.adafruit.com/datasheets/SSD1306.pdf'
+PDF_FILE='SSD1306.pdf'
+
+does_cmd_exist() {
+    CMD="$1"
+
+    command -v "$CMD" > /dev/null 2>&1
+}
+
+if does_cmd_exist curl; then
+    DL_CMD='curl'
+    DL_CMD_OPTS="--output $PDF_FILE"
+elif does_cmd_exist wget; then
+    DL_CMD='wget'
+    DL_CMD_OPTS="--no-verbose --show-progress --output-document $PDF_FILE"
+else
+    printf 'could not find a suitable downloader (curl/wget)\n'
+    exit 1
+fi
+
+# Don't double quote OPTS if you need the OPTS variable to word split.
+# You want the variable to "split" so the command's options are passed
+# in correctly
+#
+# OPTS='--first-opt --second-opt'
+#
+# If opts IS double-quoted, the command would see this
+# '--first-opt --second-opt' as its first and only argument.
+#
+# If opts ISN'T double-quoted, the command would see this
+# --first-opt --second-opt
+#
+# --first-opt being its first argument and
+# --second-opt being its second argument.
+#
+# One thing to keep in mind though, is that if OPTS can be empty, an
+# empty pair of quotes would be passed into the command. The quoting
+# makes no difference if there is only one option to pass in.
+"$DL_CMD" "$DL_CMD_OPTS" "$DATASHEET_URL"
