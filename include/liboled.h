691ca7de9bf2bd68480d6840911fc2d2d295c4ee
Jony <maybe-one-day-ubermensch@protonmail.com>
2021-09-05 12:54:48 -0700

    include: group fundamental commands under the same enum
    This will also be the case for other commands.

--- include/liboled.h
+++ include/liboled.h
@@ -6,20 +6,22 @@
  * @{
  */
 
-/**
- * Select 1 out of 256 contrast steps
- * @note You must send the contrast step right after (8 bits long)
- */
-#define LIBOLED_SET_CONTRAST_CTRL 0x81
+enum liboled_fundamental_commands {
+    /**
+     * Select 1 out of 256 contrast steps
+     * @note You must send the contrast step right after (8 bits long)
+     */
+    LIBOLED_SET_CONTRAST_CTRL = 0x81,
 
-#define LIBOLED_OUTPUT_FOLLOW_RAM 0xA4 /**< Output follows RAM content */
-#define LIBOLED_OUTPUT_IGNORE_RAM 0xA5 /**< Output ignores RAM content */
+    LIBOLED_OUTPUT_FOLLOW_RAM = 0xA4, /**< Output follows RAM content */
+    LIBOLED_OUTPUT_IGNORE_RAM = 0xA5, /**< Output ignores RAM content */
 
-#define LIBOLED_NORMAL_DISPLAY 0xA6 /**< Value of 1 represents an on pixel */
-#define LIBOLED_INVERT_DISPLAY 0xA7 /**< Value of 0 represents an on pixel */
+    LIBOLED_NORMAL_DISPLAY = 0xA6, /**< Value of 1 represents an on pixel */
+    LIBOLED_INVERT_DISPLAY = 0xA7, /**< Value of 0 represents an on pixel */
 
-#define LIBOLED_DISPLAY_OFF 0xAE /**< Turn the display off */
-#define LIBOLED_DISPLAY_ON  0xAF /**< Turn the display on */
+    LIBOLED_DISPLAY_OFF = 0xAE, /**< Turn the display off */
+    LIBOLED_DISPLAY_ON = 0xAF,  /**< Turn the display on */
+};
 
 /** @} */
 
