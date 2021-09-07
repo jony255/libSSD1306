9a8e55c3174198df9cdd2d13e0a352211a2feeea
Jony <maybe-one-day-ubermensch@protonmail.com>
2021-09-06 20:36:04 -0700

    include: use full stops in doxygen comments
    Also, rename `OUTPUT` to `DISPLAY` in the enum's definitions. The new
    name is more consistent with everything else in the file.

--- include/liboled.h
+++ include/liboled.h
@@ -7,17 +7,29 @@
  */
 
 enum liboled_fundamental_commands {
-    LIBOLED_DISPLAY_ON = 0xAF,  /**< Turn the display on */
-    LIBOLED_DISPLAY_OFF = 0xAE, /**< Turn the display off */
-
-    LIBOLED_OUTPUT_FOLLOW_RAM = 0xA4, /**< Output follows RAM content */
-    LIBOLED_OUTPUT_IGNORE_RAM = 0xA5, /**< Output ignores RAM content */
-
-    LIBOLED_NORMAL_DISPLAY = 0xA6, /**< Value of 1 represents an on pixel */
-    LIBOLED_INVERT_DISPLAY = 0xA7, /**< Value of 0 represents an on pixel */
+    LIBOLED_DISPLAY_ON = 0xAF,  /**< Turn the display on. */
+    LIBOLED_DISPLAY_OFF = 0xAE, /**< Turn the display off. */
 
     /**
-     * Select 1 out of 256 contrast steps
+     * The display shows the RAM's content.
+     */
+    LIBOLED_DISPLAY_FOLLOW_RAM = 0xA4,
+    /**
+     * The display ignores the RAM's content.
+     */
+    LIBOLED_DISPLAY_IGNORE_RAM = 0xA5,
+
+    /**
+     * A value of '1' in RAM represents an on pixel.
+     */
+    LIBOLED_NORMAL_DISPLAY = 0xA6,
+    /**
+     * A value of '0' in RAM represents an on pixel.
+     */
+    LIBOLED_INVERT_DISPLAY = 0xA7,
+
+    /**
+     * Select 1 out of 256 contrast steps.
      * @note You must send the contrast step right after (8 bits long)
      */
     LIBOLED_SET_CONTRAST_CTRL = 0x81,
