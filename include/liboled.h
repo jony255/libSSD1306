b30b631f041942c1a9eb27eaa17fef1a46eb6e5a
Jony <maybe-one-day-ubermensch@protonmail.com>
2021-09-06 20:04:58 -0700

    include: reorder enum values of `liboled_fundamental_commands`
    The order of the enums was intially based off of the order show in the
    commands table in chapter 9 of the datasheet.
    
    I gave priority to the commands responsible for turning the screen on
    first.

--- include/liboled.h
+++ include/liboled.h
@@ -7,11 +7,8 @@
  */
 
 enum liboled_fundamental_commands {
-    /**
-     * Select 1 out of 256 contrast steps
-     * @note You must send the contrast step right after (8 bits long)
-     */
-    LIBOLED_SET_CONTRAST_CTRL = 0x81,
+    LIBOLED_DISPLAY_ON = 0xAF,  /**< Turn the display on */
+    LIBOLED_DISPLAY_OFF = 0xAE, /**< Turn the display off */
 
     LIBOLED_OUTPUT_FOLLOW_RAM = 0xA4, /**< Output follows RAM content */
     LIBOLED_OUTPUT_IGNORE_RAM = 0xA5, /**< Output ignores RAM content */
@@ -19,8 +16,11 @@ enum liboled_fundamental_commands {
     LIBOLED_NORMAL_DISPLAY = 0xA6, /**< Value of 1 represents an on pixel */
     LIBOLED_INVERT_DISPLAY = 0xA7, /**< Value of 0 represents an on pixel */
 
-    LIBOLED_DISPLAY_OFF = 0xAE, /**< Turn the display off */
-    LIBOLED_DISPLAY_ON = 0xAF,  /**< Turn the display on */
+    /**
+     * Select 1 out of 256 contrast steps
+     * @note You must send the contrast step right after (8 bits long)
+     */
+    LIBOLED_SET_CONTRAST_CTRL = 0x81,
 };
 
 /** @} */
