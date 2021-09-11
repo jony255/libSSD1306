25ce2da0bd685462d914ec849ac5c7c0e1ad56aa
Jony <maybe-one-day-ubermensch@protonmail.com>
2021-09-11 11:20:49 -0700

    all: rename the library to `libSSD1306`

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# liboled
+libSSD1306
+=======
+
+Description
+-----------
+`libSSD1306` is a library meant for programming OLEDs driven by an
+`SSD1306` controller. There are other IC's that mimic the instruction set of
+the `SSD1306`. The most prominent one out there is the `SSH1106`. Getting this
+library to work with the `SSD1306` clones shouldn't require too much work.
+
+Commands
+--------
+All of the commands present in the datasheet are defined as `enum`s and grouped
+according to the table they were in. For example, `Table 9-1`'s title is
+`Fundamental Command`. Its corresponding `enum` identifier is
+`enum ssd1306_fundamental_command`.
+
+There are some commands that don't accept arguments. However, commands that do
+accept arguments, require them. For example, `SSD1306_SET_CONTRAST_CTRL` accepts
+one argument, the contrast level. This contrast level isn't optional and must be
+supplied.
--- include/liboled.h
+++ include/liboled.h
@@ -1,39 +1,39 @@
-#ifndef LIBOLED_LIBOLED_H
-#define LIBOLED_LIBOLED_H
+#ifndef LIBSSD1306_SSD1306_H
+#define LIBSSD1306_SSD1306_H
 
 /**
  * @defgroup fundamental_commands Fundamental Commands
  * @{
  */
 
-enum liboled_fundamental_command {
-    LIBOLED_DISPLAY_ON = 0xAF,  /**< Turn the display on. */
-    LIBOLED_DISPLAY_OFF = 0xAE, /**< Turn the display off. */
+enum ssd1306_fundamental_command {
+    SSD1306_DISPLAY_ON = 0xAF,  /**< Turn the display on. */
+    SSD1306_DISPLAY_OFF = 0xAE, /**< Turn the display off. */
 
     /**
      * The display shows the RAM's content.
      */
-    LIBOLED_DISPLAY_FOLLOW_RAM = 0xA4,
+    SSD1306_DISPLAY_FOLLOW_RAM = 0xA4,
     /**
      * The display ignores the RAM's content.
      */
-    LIBOLED_DISPLAY_IGNORE_RAM = 0xA5,
+    SSD1306_DISPLAY_IGNORE_RAM = 0xA5,
 
     /**
      * A value of '1' in RAM represents an on pixel.
      */
-    LIBOLED_NORMAL_DISPLAY = 0xA6,
+    SSD1306_NORMAL_DISPLAY = 0xA6,
     /**
      * A value of '0' in RAM represents an on pixel.
      */
-    LIBOLED_INVERT_DISPLAY = 0xA7,
+    SSD1306_INVERT_DISPLAY = 0xA7,
 
     /**
      * Select 1 out of 256 contrast steps.
      */
-    LIBOLED_SET_CONTRAST_CTRL = 0x81,
+    SSD1306_SET_CONTRAST_CTRL = 0x81,
 };
 
 /** @} */
 
-#endif /* LIBOLED_LIBOLED_H */
+#endif /* LIBSSD1306_SSD1306_H */
