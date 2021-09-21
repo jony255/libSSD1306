39ccabc22c073c6d23f0d7197c99482d6a64f06b
Jony <maybe-one-day-ubermensch@protonmail.com>
2021-09-21 13:05:40 -0700

    err.h: define an enum most, if not all, fns should return
    This should make error handling more easy to follow.

---
+++ include/ssd1306/err.h
@@ -0,0 +1,19 @@
+#ifndef LIBSSD1306_ERR_H
+#define LIBSSD1306_ERR_H
+
+/**
+ * @defgroup ssd1306_return_codes Possible Return Codes
+ *
+ * @{
+ */
+
+/**
+ * An enum that contains of all possible return codes.
+ */
+enum ssd1306_err {
+    SSD1306_OK, /**< The function did not encounter an error. */
+};
+
+/** @} */
+
+#endif /* LIBSSD1306_ERR_H */
