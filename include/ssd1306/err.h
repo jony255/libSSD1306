2055bc47ef8c5a236c59db0dd875e1793872d713
Jony <maybe-one-day-ubermensch@protonmail.com>
2021-10-07 16:36:51 -0700

    err.h: add macro that propagates error in fn call to caller
    Instead of writing this pattern over and over,
    
            enum ssd1306_err ret = fn_call;
    
            if (ret != SSD1306_OK) {
                return ret;
            }
    
    define a macro to take care of it. There is probably a cleaner way of
    handling errors, but, for now, this will do.

--- include/ssd1306/err.h
+++ include/ssd1306/err.h
@@ -20,6 +20,21 @@ enum ssd1306_err {
     SSD1306_READING_UNSUPPORTED,
 };
 
+/**
+ * Simple macro that propagates the return code of @c fn_call iff the return
+ * code isn't @c SSD1306_OK.
+ *
+ * @param fn_call call expression to check
+ */
+#define SSD1306_RETURN_ON_ERR(fn_call)                                         \
+    do {                                                                       \
+        enum ssd1306_err ret = (fn_call);                                      \
+                                                                               \
+        if (ret != SSD1306_OK) {                                               \
+            return ret;                                                        \
+        }                                                                      \
+    } while (0)
+
 /** @} */ /* ssd1306_return_codes */
 
 #endif /* LIBSSD1306_SSD1306_ERR_H */
